0f50e3495c7ed6a0cc603fa85f5d5235cd09991a
minim <mini.mountain1990@gmail.com>
2025-08-27 19:01:01 +0900

    20250827 旧バージョンのファイルなど削除

--- preload.js
+++
@@ -1,8 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-// レンダラープロセス向けAPIを作成
-contextBridge.exposeInMainWorld('electronAPI', {
-  sendParseResult: (result) => ipcRenderer.send('parse-result', result),
-  onUpdateSelection: (callback) => ipcRenderer.on('update-selection', (event, result) => callback(result))
-  
-});
